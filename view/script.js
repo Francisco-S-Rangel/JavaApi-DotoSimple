d04a927e09df44e4e152c29f3ac59a4149c6f920
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2023-10-01 13:19:40 -0300

    front-end being created

---
+++ view/script.js
@@ -0,0 +1,17 @@
+const url = "http://localhost:8090/task/user/1";
+
+function hideLoader(){
+    document.getElementById("Loading").style.display = "none";
+}
+
+function show(task) {
+
+    let tab = `
+    <thead>
+    <th scope="col">#</th>
+    <th scope="col">Decription</th>
+    <th scope="col">username</th>
+    <th scope="col">userId</th>
+    </thead>`
+
+}
